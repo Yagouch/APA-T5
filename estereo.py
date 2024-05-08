8afa7ef526b3e78d07a58391478b4962de613df3
Yagouch <yagocarballo@estudiantat.upc.edu>
2024-05-09 00:15:48 +0200

    2/4 functions working

--- estereo.py
+++ estereo.py
@@ -6,16 +6,10 @@ Module description: Audio signal handling, stereo and mono converter, signal enc
 def getData(file):
     # Leer archivo 'file'
     f = open(file,'rb')
-    f.read(44) # Selecciono solo los bytes de datos (A partir del 44 según: http://soundfile.sapp.org/doc/WaveFormat/)
     
-    data = []
-    while True:
-        muestra = f.read(1) 
-
-        if not muestra:
-            break
-
-        data += muestra
+    # Selecciono solo los bytes de datos (A partir del 44 según: http://soundfile.sapp.org/doc/WaveFormat/)
+    f.read(44)
+    data = list(f.read())
 
     return data
 
@@ -25,24 +19,18 @@ def setHeader(original_file, data, mono=1):
     headerData = list(f.read(44))
     if mono:
         headerData[22] = 1 # NumChannels ([22:24] pero al ser little endian con modificar el primero podemos alternar mono/stereo)
-        
-        # SampleRate * 2
-        # fs_str = ''.join(format(num, '02x') for num in headerData[27:23:-1])
-        # fs = int(fs_str, 16) * 2
-        # fs_doble_hex = format(fs, '08x')
-
-        # fs_doble = [int(fs_doble_hex[i:i+2],16) for i in range(len(fs_doble_hex)-2, -1, -2)]
-
-        # headerData[24:28] = fs_doble
-
     else:
         headerData[22] = 2 # stereo
     
-
     headerData += data
 
     return headerData
 
+def writeData(file, name):
+    audio_bytes = bytes(file)
+
+    with open(name, 'wb') as archivo:
+        archivo.write(audio_bytes)
 
 def estereo2mono(ficEste, ficMono, canal=2):
     data = getData(ficEste)
@@ -50,22 +38,28 @@ def estereo2mono(ficEste, ficMono, canal=2):
     if canal == 0:
         data_ficMono = []
         [data_ficMono.extend(data[i:i+2]) for i in range(0,len(data), 4)]
-    # elif canal == 1:
-    #     data_ficMono = [muestra[2:4] for muestra in data]
-    # elif canal == 2:
-    #     data_ficMono = [((muestra[0] + muestra[2]) / 2, (muestra[1] + muestra[3]) / 2) for muestra in data]
-    # elif canal == 3:
-    #     data_ficMono = [((muestra[0] - muestra[2]) / 2, (muestra[1] - muestra[3]) / 2) for muestra in data]
+
+        audio_mono = setHeader(ficEste, data_ficMono, mono=1)
+    elif canal == 1:
+        data_ficMono = []
+        [data_ficMono.extend(data[i:i+2]) for i in range(2,len(data), 4)]
+
+        audio_mono = setHeader(ficEste, data_ficMono, mono=1)
+    elif canal == 2:
+        data_ficMono = []
+        [data_ficMono.extend([int(data[i]/2+data[i+2]/2), int(data[i+1]/2+data[i+3]/2)]) for i in range(0,len(data), 4)]
+
+        audio_mono = setHeader(ficEste, data_ficMono, mono=1)
+    elif canal == 3:
+        data_ficMono = []
+        [data_ficMono.extend([abs(int(data[i]/2-data[i+2]/2)), abs(int(data[i+1]/2-data[i+3]/2))]) for i in range(0,len(data), 4)]
+
+        audio_mono = setHeader(ficEste, data_ficMono, mono=1)
+
     else:
         print("Debe introduir un canal válido.")
-    audio_mono = setHeader(ficEste, data_ficMono, mono=1)
-    
-    audio_bytes = bytes(audio_mono)
 
-    with open(ficMono, 'wb') as archivo:
-        archivo.write(audio_bytes)
-
-
+    writeData(audio_mono, ficMono)
 
 
 def mono2estereo(ficIzq, ficDer, ficEste):
@@ -73,9 +67,13 @@ def mono2estereo(ficIzq, ficDer, ficEste):
     data_izq = getData(ficIzq)
     data_der = getData(ficDer)
     
-    data = [sum(muestra, ()) for muestra in zip(data_izq, data_der)]
+    data_Este = []
 
-    ficEste = setHeader(ficIzq, data, mono=0)
+    [data_Este.extend([data_izq[i], data_izq[i+1], data_der[i], data_der[i+1]]) for i in range(0, len(data_der), 2)]
+
+    audio_Este = setHeader(ficIzq, data_Este, mono=0)
+
+    writeData(audio_Este, ficEste)
 
 
 def codEstereo(ficEste, ficCod):
@@ -90,7 +88,14 @@ def codEstereo(ficEste, ficCod):
 
 # def decEstereo(ficCod, ficEste)
 
+if __name__ == '__main__':
+
+    estereo2mono('wav/komm.wav', 'wav/komm_e2m0.wav', canal=0)
+    estereo2mono('wav/komm.wav', 'wav/komm_e2m1.wav', canal=1)
+    estereo2mono('wav/komm.wav', 'wav/komm_e2m2.wav', canal=2)
+    estereo2mono('wav/komm.wav', 'wav/komm_e2m3.wav', canal=3)
+
+    mono2estereo('wav/komm_e2m0.wav', 'wav/komm_e2m1.wav', 'wav/komm_m2e.wav')
 
 
-estereo2mono('wav/komm.wav', 'test.wav', canal=0)
 
